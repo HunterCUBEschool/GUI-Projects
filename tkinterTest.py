ecea64d73042433803e139b3dc1c05f20890b465
HunterCUBEschool <79872738+HunterCUBEschool@users.noreply.github.com>
2021-05-11 13:32:21 -0600

    Add files via upload

--- tkinterTest.py
+++ tkinterTest.py
@@ -3,10 +3,6 @@ from tkinter import *
 top = Tk()
 songList = []
 
-def addTrack():
-    songList.append(E1.get())
-    E1.delete(0, END)
-
 def printList():
     print(songList)
 
@@ -27,13 +23,16 @@ def mainMenu():
 
     B1main = Button(text = "Week 1", bg = "#23d108", command= week1)
     B1main.grid(column = 0, row = 1)
-    B2main = Button(text = "Week 2", bg = "#0884d1", )
+    B2main = Button(text = "Week 2", bg = "#0884d1", command= week2)
     B2main.grid(column = 0, row = 2)
     B3main = Button(text = "Week 3", bg = "#9b08d1", )
     B3main.grid(column = 0, row = 3)
 
 def week1():
-    clearWindow()
+    def addTrack():
+        songList.append(E1.get())
+        E1.delete(0, END)
+        clearWindow()
     #label
     L1 = Label(top, text="Playlist Generator")
     L1.grid(column= 0, row= 1)
@@ -52,8 +51,26 @@ def week1():
     B3.grid(column = 1, row = 3)
 
 
+def week2():
+    clearWindow()
 
-    print(E1.get())
+    L1W2 = Label(top, text="Number of Rolls")
+    L1W2.grid(column= 0, row= 1)
+    
+    L2W2 = Label(top, text="Number of Sides")
+    L2W2.grid(column= 2, row= 1)
+              
+    L3W2= Label(top, text="Dice Roller App")
+    L3W2.grid(column= 1, row= 0)
+    
+    E1W2 = Entry(top, bd = 5)
+    E1W2.grid(column= 0, row= 2)
+    
+    E2W2 = Entry(top, bd = 5)
+    E2W2.grid(column= 2, row= 2)
+    
+    B1W2 = Button(text= "Roll Dice", bg = "Yellow")
+    B1W2.grid(column = 1, row = 4)
 
 if __name__ == "__main__":
     mainMenu()
